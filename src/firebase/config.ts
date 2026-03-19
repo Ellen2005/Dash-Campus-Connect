34add5230fc34f7cc2d60853d9a80020317ba62f
Ngu Nadia Mbuo <ngunadiambuo@gmail.com>
2026-03-19 12:19:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9174282878-98994",
+  "appId": "1:851041789670:web:baba10bbfb40ea27d87d27",
+  "apiKey": "AIzaSyCuWDS_64e2EzNZ8aJdmug2PRSejnVchNo",
+  "authDomain": "studio-9174282878-98994.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "851041789670"
+};
